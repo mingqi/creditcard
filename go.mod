95804e6535a4a26e41297348eaa73110c2a8d2ab
xulei <xulei@xuleideMacBook-Air.local>
2022-09-16 21:13:57 -0700

    save

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module example/hello
 
 go 1.18
 
-require github.com/google/go-github/v45 v45.2.0
+require github.com/wcharczuk/go-chart/v2 v2.1.0
 
 require (
-	github.com/google/go-querystring v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
 )
--- hello.go
+++ hello.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
+	"github.com/wcharczuk/go-chart/v2"
 )
 
 type Person struct {
@@ -26,33 +28,41 @@ func findPeople(name string, people []Person) Person {
 	return theperson
 }
 func main() {
-	csvFile, err := os.Open("/Users/xulei/creditcard/may.csv")
-	if err != nil {
-		fmt.Println(err)
+	/*
+		fmt.Println("-----------------------------")
+		txList, err := tolist("/Users/xulei/creditcard/may.csv")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("-----------------------------")
+		for _, t := range txList {
+			fmt.Println("TX:")
+			fmt.Println(t.amount)
+			fmt.Println(t.payee)
+			fmt.Println(t.category)
+		}
+		category_output := tocategory(txList)
+		fmt.Println(calculate(category_output))
+	*/
+	pie := chart.PieChart{
+		Width:  512,
+		Height: 512,
+		Values: []chart.Value{
+			{Value: 5, Label: "Blue"},
+			{Value: 5, Label: "Green"},
+			{Value: 4, Label: "Gray"},
+			{Value: 4, Label: "Orange"},
+			{Value: 3, Label: "Deep Blue"},
+			{Value: 3, Label: "??"},
+			{Value: 1, Label: "!!"},
+		},
 	}
-	fmt.Println("Successfully Opened CSV file")
-	defer csvFile.Close()
 
-	csvLines, err := csv.NewReader(csvFile).ReadAll()
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, line := range csvLines {
-		fmt.Println(line[0])
-		//t := Tx{
-		//payee:    "fff",
-		//amount:   10,
-		//category: "house",
-	}
-	// fmt.Println(tolist("/Users/xulei/creditcard/may.csv"))
-	fmt.Println("-----------------------------")
-	txList := tolist("/Users/xulei/creditcard/may.csv")
-	for _, t := range txList {
-		fmt.Println("TX:")
-		fmt.Println(t.amount)
-		fmt.Println(t.payee)
-		fmt.Println(t.category)
-	}
+	f, _ := os.Create("output.png")
+	defer f.Close()
+	pie.Render(chart.PNG, f)
+
 }
 
 type Tx struct {
@@ -61,19 +71,19 @@ type Tx struct {
 	category string  // ""
 }
 
-func tolist(filepath string) []Tx {
+func tolist(filepath string) ([]Tx, error) {
 	var txlist []Tx
 	var firstline []string
 	csvFile, err := os.Open(filepath)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	fmt.Println("Successfully Opened CSV file")
 	defer csvFile.Close()
 
 	csvLines, err := csv.NewReader(csvFile).ReadAll()
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	for _, line := range csvLines {
 		firstline = line
@@ -92,8 +102,7 @@ func tolist(filepath string) []Tx {
 				if line[i] != "Amount" {
 					f, err := strconv.ParseFloat(line[i], 32)
 					if err != nil {
-						fmt.Println(err)
-						continue
+						return nil, err
 					}
 					tx.amount = float32(f)
 				}
@@ -109,5 +118,34 @@ func tolist(filepath string) []Tx {
 		}
 
 	}
-	return txlist
+	return txlist, nil
+}
+func tocategory(txlist []Tx) map[string][]Tx {
+	txmap := make(map[string][]Tx)
+	for _, tx := range txlist {
+		if _, ok := txmap[tx.category]; !ok {
+			txmap[tx.category] = []Tx{}
+		}
+		txmap[tx.category] = append(txmap[tx.category], tx)
+	}
+	return txmap
+}
+func calculate(categoryoutput map[string][]Tx) map[string]float32 {
+	catPercMap := make(map[string]float32)
+	var totalamount float32
+	for _, value := range categoryoutput {
+		for _, tx := range value {
+			totalamount = totalamount + -tx.amount
+		}
+	}
+	for key, value := range categoryoutput {
+		var totalcategory float32
+		for _, tx := range value {
+			totalcategory = totalcategory + -tx.amount
+		}
+		categoryPercent := totalcategory / totalamount * 100
+		catPercMap[key] = categoryPercent
+		fmt.Println(totalamount)
+	}
+	return catPercMap
 }
